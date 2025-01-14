773202381e67c6de191078e2efbcfa9f72223ce3
Pablo Nudel <pablonudel@gamil.com>
2025-01-14 17:42:51 +0100

    refactor: simplify output messages using f-strings

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number, "is positive", end="\n")
+    print(f"{number} is positive")
 elif number == 0:
-    print(number, "is zero", end="\n")
+    print(f"{number} is zero")
 else:
-    print(number, "is negative", end="\n")
+    print(f"{number} is negative")
