2723c20359f68181ad6f3b686c5a177d2ce09802
Pablo Nudel <pablonudel@gamil.com>
2025-01-14 17:33:55 +0100

    fix: standardize line endings in output messages

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number, "is positive", end='\n')
+    print(number, "is positive", end="\n")
 elif number == 0:
-    print(number, "is zero", end='\n')
+    print(number, "is zero", end="\n")
 else:
-    print(number, "is negative", end='\n')
+    print(number, "is negative", end="\n")
