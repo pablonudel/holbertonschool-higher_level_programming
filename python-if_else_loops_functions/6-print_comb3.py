05ca4136a630e0ee26b79ed2f58dcdb58da5058c
Pablo Nudel <pablonudel@gamil.com>
2025-01-15 16:58:39 +0100

    correct print formatting in combination generator

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
+        print("{}".format(i), end='')
         if i == 8 and j == 9:
-            print(f"{i}{j}")
+            print("{}".format(j))
         else:
-            print(f"{i}{j}", end=", ")
+            print("{}".format(j), end=", ")
