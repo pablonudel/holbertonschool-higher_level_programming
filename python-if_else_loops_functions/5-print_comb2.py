c8efc55018bdb1d5d7cef39f1e568204997a9bb5
Pablo Nudel <pablonudel@gamil.com>
2025-01-15 16:37:15 +0100

    script to print combinations of two-digit numbers

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for i in range(0, 100):
+    if i == 99:
+        i = "{:02d}".format(i)
+        end = ''
+    else:
+        i = "{:02d}".format(i)
+        end = ", "
+    print(i, end=end)
