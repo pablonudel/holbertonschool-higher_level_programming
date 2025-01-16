a1143194597f73ac9c150b25b09bee4208a0dd42
Pablo Nudel <pablonudel@gamil.com>
2025-01-16 10:16:50 +0100

    enhance argument handling in command-line script to display argument count and values

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    from args_2 import args
+    argc = len(sys.argv)
+    argv = sys.argv
 
-    args(len(sys.argv), sys.argv)
+    if argc == 1:
+        print("0 arguments.")
+    elif argc == 2:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(argc - 1))
+    for i in range(1, argc):
+        print("{:d}: {:s}".format(i, argv[i]))
