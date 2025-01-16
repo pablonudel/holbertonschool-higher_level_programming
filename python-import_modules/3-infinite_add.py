e9de221e6cc8c5383cd40515d039e1c6d95eeeca
Pablo Nudel <pablonudel@gamil.com>
2025-01-16 10:50:28 +0100

    move import and argument handling inside main guard

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv)
-argv = sys.argv
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    argv = sys.argv
 
-for i in range(1, argc):
-    argv[i] = int(argv[i])
-print(sum(argv[1:]))
+    for i in range(1, argc):
+        argv[i] = int(argv[i])
+    print(sum(argv[1:]))
