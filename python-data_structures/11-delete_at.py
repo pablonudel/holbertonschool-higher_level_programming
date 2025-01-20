7f4e49c066b9cb35ac0d4753a112905bc32c1845
Pablo Nudel <pablonudel@gamil.com>
2025-01-20 14:02:33 +0100

    delete_at function to handle negative indices and simplify removal logic

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx > 0 and idx < len(my_list):
-        for i in range(len(my_list)):
-            if i == idx:
-                my_list.remove(my_list[idx])
+    if idx >= 0 and idx < len(my_list):
+        my_list.remove(my_list[idx])
 
     return my_list
