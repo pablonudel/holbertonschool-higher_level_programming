576c44c71804acf38d9c13f15ea0de8e4df28049
Pablo Nudel <pablonudel@gamil.com>
2025-01-20 13:58:39 +0100

    delete_at function to correctly remove element at specified index

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if len(my_list):
-        if idx > 0 and idx < len(my_list):
-            my_list.remove(my_list[idx])
+    if idx > 0 and idx < len(my_list):
+        for i in range(len(my_list)):
+            if i == idx:
+                my_list.remove(my_list[idx])
 
     return my_list
