ca60d4fa7b694d11b7168e2916c3894a74c32b1c
Pablo Nudel <pablonudel@gamil.com>
2025-01-21 13:16:29 +0100

    improve handling of Roman numeral subtraction logic

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     res = 0
-    if roman_string is not None and isinstance(roman_string, str):
+    if roman_string and isinstance(roman_string, str):
         rti = {'M': 1000, 'D': 500, 'C': 100, 'L': 50,
                'X': 10, 'V': 5, 'I': 1}
         for i in range(len(roman_string)):
             if i > 0 and rti[roman_string[i]] > rti[roman_string[i - 1]]:
-                res += rti[roman_string[i]] - 2
+                res += rti[roman_string[i]] - (rti[roman_string[i - 1]] * 2)
             else:
                 res += rti[roman_string[i]]
     return res
