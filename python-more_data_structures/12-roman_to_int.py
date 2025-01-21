ced3b0aeb7a0017252d19da2bd5a384e51152c1b
Pablo Nudel <pablonudel@gamil.com>
2025-01-21 13:09:19 +0100

    handle None input

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     res = 0
-    if roman_string and isinstance(roman_string, str):
+    if roman_string is not None and isinstance(roman_string, str):
         rti = {'M': 1000, 'D': 500, 'C': 100, 'L': 50,
                'X': 10, 'V': 5, 'I': 1}
         for i in range(len(roman_string)):
