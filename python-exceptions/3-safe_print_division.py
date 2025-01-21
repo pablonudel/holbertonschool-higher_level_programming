f80e4bfc75e713b1cfd11b41ea7eaf96d090c85d
Pablo Nudel <pablonudel@gamil.com>
2025-01-21 17:46:03 +0100

    simplify output handling

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,8 +5,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         res = None
     finally:
-        if res:
-            print("Inside result: {:.1f}".format(res))
-        else:
-            print("Inside result: {}".format(res))
-    return res
+        print("Inside result: {}".format(res))
+        return res
