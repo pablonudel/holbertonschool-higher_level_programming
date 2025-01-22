446507984cd083f99ac2c18bcb520e8d1c381b87
Pablo Nudel <pablonudel@gamil.com>
2025-01-22 12:49:14 +0100

    update to clarify return type in docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,6 +9,7 @@ or floats before performing the addition.
 def add_integer(a, b=98):
     """
     Adds two numbers after validating their types.
+    Returns the result as integer.
     """
     if isinstance(a, (int, float)) is False:
         raise TypeError("a must be an integer")
