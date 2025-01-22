1dc8f656295cc448e29f465da16b849053176898
Pablo Nudel <pablonudel@gamil.com>
2025-01-22 16:11:30 +0100

    add docstrings to function and module

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+This module provides a function that prints a text
+with 2 new lines after each of these characters: ., ? and :
+<text> must be a string.
+There should be no space at the beginning or at the end of each printed line.
+"""
+
+
 def text_indentation(text):
+    """
+    function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    """
     if isinstance(text, str) is False:
         raise TypeError("text must be a string")
 
