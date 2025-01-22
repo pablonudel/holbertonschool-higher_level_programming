854d19319a39fb24bc1a364e2aa135eab32ea42e
Pablo Nudel <pablonudel@gamil.com>
2025-01-22 10:48:01 +0100

    function to validate and add two integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module provides a function to add two numbers
+after validating their types. It ensures inputs are integers
+or floats before performing the addition.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two numbers after validating their types.
+    """
+    if isinstance(a, (int, float)) is False:
+        raise TypeError("a must be an integer")
+    if isinstance(b, (int, float)) is False:
+        raise TypeError("b must be an integer")
+    return int(a + b)
