479140875bc78122bdd8438f8c6800ddf21fa680
Pablo Nudel <pablonudel@gamil.com>
2025-01-22 13:54:36 +0100

    function to print formatted name with type checks

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module provides a function that prints
+"My name is <first name> <last name>" were
+<first name> and <last name> must be strings
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    function that prints:
+    My name is <first name> <last name>
+    """
+    if isinstance(first_name, str) is False:
+        raise TypeError("first_name must be a string")
+    if isinstance(last_name, str) is False:
+        raise TypeError("last_name must be a string")
+    
+    print("My name is {} {}".format(first_name, last_name))
