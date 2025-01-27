1922a3a1607680b51d32b6b357f290b3b5c8c30e
Pablo Nudel <pablonudel@gamil.com>
2025-01-27 13:03:19 +0100

    Square class with size property, validation, and area calculation

---
+++ python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Square:
+    __size = None
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+
+    def __init__(self, size=0):
+        self.__size = size
