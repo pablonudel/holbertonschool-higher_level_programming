097734b7c2252dc5631e8dde1422c1fa043d12ea
Pablo Nudel <pablonudel@gamil.com>
2025-01-27 13:07:35 +0100

    Add docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module creates an empty class Square that defines a square.
+"""
 class Square:
+    """ 
+    This is an empty class Square that defines a square.
+    """
     def __init__(self):
         self
