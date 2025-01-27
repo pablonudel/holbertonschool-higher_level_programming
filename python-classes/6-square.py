2fef168a644599d0b3ebfad1cfea4e19e6910d6d
Pablo Nudel <pablonudel@gamil.com>
2025-01-27 13:36:45 +0100

    Simplify print method in Square class by removing redundant code

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -48,11 +48,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
+            for i in range(self.__position[1]):
                 print()
             for i in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
-
-    def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
