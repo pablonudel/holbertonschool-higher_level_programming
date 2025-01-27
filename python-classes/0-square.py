c4045ba088997fb8a3a4da10325145bfae571a09
Pablo Nudel <pablonudel@gamil.com>
2025-01-27 13:09:07 +0100

    Fix docstring formatting

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,8 +2,10 @@
 """
 This module creates an empty class Square that defines a square.
 """
+
+
 class Square:
-    """ 
+    """
     This is an empty class Square that defines a square.
     """
     def __init__(self):
