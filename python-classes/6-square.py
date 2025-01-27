d73e9d59f8a57e07cb910e54efbd66733992f7ea
Pablo Nudel <pablonudel@gamil.com>
2025-01-27 13:03:36 +0100

    Square class with size and position properties, validation, and area calculation

---
+++ python-classes/6-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+class Square:
+    __size = None
+    __position = None
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise ValueError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(i, int) for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
