e8c93dc91f6d3fe27aba47b2e4d3f3b419d006e3
Pablo Nudel <pablonudel@gamil.com>
2025-01-27 13:02:48 +0100

    Square class with size validation and area calculation

---
+++ python-classes/3-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    __size = None
+
+    def area(self):
+        return self.__size ** 2
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
