1873f1331dc9184c83377bda2dab13c32f0aa5e4
Pablo Nudel <pablonudel@gamil.com>
2025-01-27 13:43:40 +0100

    Update __init__ method in Square class to use public attributes for size and position

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,8 +13,8 @@ class Square:
     Print method to print the square
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         return self.__size ** 2
