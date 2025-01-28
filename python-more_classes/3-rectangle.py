115711d9e295380fccecd8624f4543d2267312fc
Pablo Nudel <pablonudel@gamil.com>
2025-01-28 09:19:03 +0100

    Rectangle class with width, height, area, and perimeter methods

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""This module creates class Rectangle that defines a rectangle."""
+
+
+class Rectangle:
+    """
+    class Rectangle that defines a rectangle.
+    Properties: width and height.
+    Area and Perimeter methods.
+    Print and str methods.
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        for i in range(self.__height):
+            print("#" * self.__width,
+                  end=('' if i == self.__height - 1 else '\n'))
+        return ''
