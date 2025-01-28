99f9b2543c7bc1de1e886e6cec7ac186afb19739
Pablo Nudel <pablonudel@gamil.com>
2025-01-28 09:18:04 +0100

    Rectangle class to define a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module creates class Rectangle that defines a rectangle."""
+
+
+class Rectangle:
+    """class Rectangle that defines a rectangle."""
+    def __init__(self):
+        self
