1f2fac5dfc57383fdb82f9741cb261716436315b
Pablo Nudel <pablonudel@gamil.com>
2025-02-04 12:03:44 +0100

    Add Square class that inherits from Rectangle with size validation and area method

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module for the Square class """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class that inherits from Rectangle """
+    def __init__(self, size):
+        """ Method that initializes the square """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """ Method that returns the area of the square """
+        return self.__size ** 2
