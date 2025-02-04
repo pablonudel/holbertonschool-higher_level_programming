6d92803e35dbed8db8370158e3c58f31618ca22c
Pablo Nudel <pablonudel@gamil.com>
2025-02-04 13:11:51 +0100

    docstrings to Animal, Dog, and Cat classes for clarity

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
+""" Abstract Base Class """
 from abc import ABC, abstractmethod
 
+
 class Animal(ABC):
+    """ Animal class """
     @abstractmethod
     def sound(self):
+        """ Sound method """
         pass
 
+
 class Dog(Animal):
+    """ Dog class """
     def sound(self):
+        """ Sound method """
         return "Bark"
 
+
 class Cat(Animal):
+    """ Cat class """
     def sound(self):
+        """ Sound method """
         return "Meow"
