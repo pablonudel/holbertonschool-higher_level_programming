2443bef0b131acc59f58428f57dac40ef4efd361
Pablo Nudel <pablonudel@gamil.com>
2025-02-04 13:11:37 +0100

    Duck Typing implementation with Shape, Circle, and Rectangle classes

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" Duck Typing """
+import math
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    """ Shape class """
+    @abstractmethod
+    def area(self):
+        """ Area method """
+        pass
+
+    @abstractmethod
+    def Perimeter(self):
+        """ Perimeter method """
+        pass
+
+
+class Circle(Shape):
+    """ Circle class """
+    def __init__(self, radius):
+        """ Circle constructor """
+        self.radius = radius
+
+    def area(self):
+        """ Area method """
+        return math.pi * self.radius ** 2
+
+    def Perimeter(self):
+        """ Perimeter method """
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """ Rectangle class """
+    def __init__(self, width, height):
+        """ Rectangle constructor """
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """ Area method """
+        return self.width * self.height
+
+    def Perimeter(self):
+        """ Perimeter method """
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    """ Shape info function """
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.Perimeter()))
