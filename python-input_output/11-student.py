349a307810b0d30ab3401d958534c3eb5121dc67
Pablo Nudel <pablonudel@gamil.com>
2025-02-10 19:48:11 +0100

    Refactor to_json method in Student class for improved attribute handling

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -12,17 +12,13 @@ class Student:
 
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a Student instance"""
-        if (isinstance(attrs, list) and
-                all(isinstance(attr, str) for attr in attrs)):
-            reversed_attrs = attrs[::-1]
-            return {
-                key: getattr(self, key)
-                for key in reversed_attrs
-                if hasattr(self, key)
-            }
-        else:
-            reversed_dict_keys = list(self.__dict__.keys())[::-1]
-            return {key: self.__dict__[key] for key in reversed_dict_keys}
+        if attrs is None:
+            return {key: self.__dict__[key] for key in sorted(self.__dict__)}
+        new_dict = {}
+        for key in sorted(attrs):
+            if key in self.__dict__:
+                new_dict[key] = self.__dict__[key]
+        return new_dict
 
     def reload_from_json(self, json):
         """replaces all attributes of the Student instance"""
