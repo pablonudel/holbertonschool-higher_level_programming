10af7912ddee1051dba687f8f733f0a97cc6ceb8
Pablo Nudel <pablonudel@gamil.com>
2025-02-10 10:55:57 +0100

    Add filename check in read_file function before opening the file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout """
-    with open(filename, encoding="utf-8") as f:
-            print(f.read(), end='')
+    if filename != "":
+        with open(filename, encoding="utf-8") as f:
+                print(f.read(), end='')
