7313de0359b78c94da941cc2cd143e6284e4983d
Pablo Nudel <pablonudel@gamil.com>
2025-02-10 17:17:10 +0100

    Add Student class with initialization and to_json method for dictionary representation

---
+++ python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for Student class"""
+
+
+class Student:
+    """Student class"""
+    def __init__(self, first_name, last_name, age):
+        """init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs == None:
+            return {key: self.__dict__[key] for key in sorted(self.__dict__)}
+        new_dict = {}
+        for key in sorted(attrs):
+            if key in self.__dict__:
+                new_dict[key] = self.__dict__[key]
+        return new_dict
