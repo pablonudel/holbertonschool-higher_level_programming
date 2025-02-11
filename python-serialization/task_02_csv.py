26795be50f3c647911632db3e51269dfd7b2c837
Pablo Nudel <pablonudel@gamil.com>
2025-02-11 12:20:04 +0100

    Fix file opening mode in convert_csv_to_json method and improve error handling

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,11 +8,12 @@ def convert_csv_to_json(csv_file):
     """takes the CSV filename as its parameter
     and writes the JSON data to data.json"""
     try:
-        with open(csv_file, newline='', encoding='utf-8') as f:
+        with open(csv_file, 'r', encoding='utf-8') as f:
             data = csv.DictReader(f)
             obj = [row for row in data]
         with open("data.json", 'w', encoding="utf-8") as f:
             json.dump(obj, f, indent=4)
-    except (FileNotFoundError, csv.Error, EOFError, IOError) as e:
+        return True
+    except (FileNotFoundError, csv.Error, EOFError, IOError, Exception) as e:
         print("Error: {}".format(e))
         return False
