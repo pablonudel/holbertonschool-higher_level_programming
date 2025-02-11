1557b0a6c7036062528215c85eb6eac21ceacdcf
Pablo Nudel <pablonudel@gamil.com>
2025-02-11 08:07:14 +0100

    Fix loop create_row function

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,8 +10,8 @@ def pascal_triangle(n):
 
     def create_row(prev_row):
         row = [1]
-        for j in range(1, len(prev_row)):
-            row.append(prev_row[j - 1] + prev_row[j])
+        for i in range(1, len(prev_row)):
+            row.append(prev_row[i - 1] + prev_row[i])
         row.append(1)
         return row
 
