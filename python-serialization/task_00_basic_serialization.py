a8c43c78793040a657aa261c75f0cb31f56f610f
Pablo Nudel <pablonudel@gamil.com>
2025-02-11 09:47:56 +0100

    Enhance serialization functions with docstrings and improve data handling

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""Module for serialize_and_save_to_file and load_and_deserialize methods"""
 import json
 
+
 def serialize_and_save_to_file(data, filename):
+    """serialize a Python dictionary to a JSON file"""
     with open(filename, 'w+', encoding="utf-8") as f:
-        f.write(json.dumps(data))
+        data = json.dumps(data)
+        f.write(data)
     pass
 
+
 def load_and_deserialize(filename):
+    """deserialize the JSON file to recreate the Python Dictionary"""
     with open(filename, 'r', encoding="utf-8") as f:
         data = f.read()
         return json.loads(data)
