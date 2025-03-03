5e01de5c5511bfaeb187e6272e313974ec3c7192
Pablo Nudel <pablonudel@gamil.com>
2025-03-03 14:05:41 +0100

    Add ORDER BY clause to SQL query for better result sorting

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- lists the number of records with the same score in the table second_table
 SELECT score, count(*) as 'number' 
 FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY score DESC;
