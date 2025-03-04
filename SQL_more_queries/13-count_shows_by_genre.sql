ba347ba73d3b412026c60aab36485d92b6a5613e
Pablo Nudel <pablonudel@gamil.com>
2025-03-04 14:41:02 +0100

    Refactor SQL query to group by genre for consistency and clarity

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,5 @@ SELECT name AS genre,
     COUNT(DISTINCT tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY name
+GROUP BY genre
 ORDER BY number_of_shows DESC;
