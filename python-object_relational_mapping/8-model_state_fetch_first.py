aaa0bd733c383bab24bd0a4507b3f40ddd2f4ca4
Pablo Nudel <pablonudel@gamil.com>
2025-03-05 18:13:57 +0100

    Add script to fetch and display the first state from the database

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
