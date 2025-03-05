e331943810dc32c065885923cdcbf2b2856b0581
Pablo Nudel <pablonudel@gamil.com>
2025-03-05 12:33:40 +0100

    Refactor state query script to simplify connection handling and enforce binary comparison for state names

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,41 +4,19 @@ import MySQLdb
 import sys
 
 
-def query_states(mysql_username, mysql_password, database):
-    """Access to database for listing the states
-    starting with letter 'N' from table states"""
-    try:
-        conn = MySQLdb.connect(host="localhost",
-                               port=3306,
-                               user=mysql_username,
-                               passwd=mysql_password,
-                               db=database,
-                               charset="utf8")
-        cur = conn.cursor()
-        cur.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-            )
-        query_rows = cur.fetchall()
-        for row in query_rows:
-            print(row)
-        cur.close()
-        conn.close()
-    except MySQLdb.Error as e:
-        print(f"MySQL error: {e}")
-    except Exception as e:
-        print(f"Unexpected error: {e}")
-
-
 if __name__ == "__main__":
-    """Main execution method"""
-    if len(sys.argv) != 4:
-        print(
-            "Usage: {} <mysql_username> <mysql_password> <database_name>"
-            .format(format(sys.argv[0])))
-        sys.exit(1)
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database = sys.argv[3]
-
-    query_states(mysql_username, mysql_password, database)
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
+        )
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
