0d70918ce03e711f833cf52f85cb59624557c893
Pablo Nudel <pablonudel@gamil.com>
2025-03-05 18:14:18 +0100

    Add script to insert a new state into the database and retrieve its ID

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Start link class to table in database"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    session.add(State(name="Louisiana"))
+    session.commit()
+
+    try:
+        state = session.query(State.id).filter_by(name="Louisiana").one()
+        print(state[0])
+    except NoResultFound:
+        print("Not found")
+
+    session.close()
