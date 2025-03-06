ac14218fe78704989cb95b2ed0505387d7f25f79
Pablo Nudel <pablonudel@gamil.com>
2025-03-06 12:45:53 +0100

    Fix password argument in database connection to use the second command line argument

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost",
                            port=3306,
                            user=sys.argv[1],
-                           passwd=sys.argv[1],
+                           passwd=sys.argv[2],
                            db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
